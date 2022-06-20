304c14142f0c041ca6acb66c697793019c38197b
Emmi L <emmi.lahtisalo@awacademy.training>
2022-06-20 15:35:04 +0300

    editing the while-loop, still a problem with quitting the program

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-process.stdout.write("Welcome to the Harry Potter Pet quiz!");
+"use strict";
 
-let score = 0
+let game = (userInput) => {
+  process.stdout.write("Welcome to the Harry Potter Pet quiz! \n");
 
-while (true) {
-    let input = userInput.toString().trim()
-    if (input === "q") {
-        process.stdout.write(`Thanks for playing!\n Here is your score: ${score}`)
-        return
+  let score = 0;
+
+  process.stdout.write("Do you want to play? Write 'yes' or 'no'");
+  let input = userInput.toString().trim();
+
+  if (input === "no") {
+    process.stdout.write("> Game over!\n");
+    process.exit();
+  }
+
+  if (input === "yes") {
+    while (true) {
+      let input = userInput.toString().trim();
+
+      if (input === "q") {
+        process.stdout.write(
+          `Thanks for playing!\n Here is your score: ${score}`
+        );
+        process.exit();
+      }
     }
-}
+  }
+};
+
+process.stdin.on("data", game);
