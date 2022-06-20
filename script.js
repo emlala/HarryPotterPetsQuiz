5d2d62f853ccf14893eb12234042221965cfe8d0
Emma King <emmailonaking@gmail.com>
2022-06-20 15:21:40 +0300

    added questions and answers

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-process.stdout.write("Welcome to the Harry Potter Pet quiz!");
+const questions = {
+    1: "What is the name of Harry's owl?",
+    2: "What is the name of Ron's rat?",
+    3: "What is the name of Hermione's cat?",
+    4: "What is the name of Hagrid's three headed dog?"
+}
+
+const answers = {
+    1: "Hedwig",
+    2: "Scabbers",
+    3: "Crookshanks",
+    4: "Fluffy"
+}
+
+console.log(questions[1])
+console.log(answers[1])
+
+process.stdout.write("Welcome to the Harry Potter Pet quiz! Press q to quit.\n")
 
 let score = 0
 
 while (true) {
-    let input = userInput.toString().trim()
-    if (input === "q") {
+    let input2 = userInput.toString().trim()
+    if (input2 === "q") {
         process.stdout.write(`Thanks for playing!\n Here is your score: ${score}`)
         return
     }
